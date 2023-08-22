a5674028b0a44a418a61723ae595b42ba98b1885
Justyn Rodrigues <justynrodrigues@hotmail.com.au>
2023-08-22 10:34:46 +1000

    now it stores for each week

--- code.R
+++ code.R
@@ -16,8 +16,7 @@ nfl <- read_csv('data/games.csv', show_col_types = FALSE) |>
     outcome = case_when(
       home_score > away_score ~ 1,
       away_score > home_score ~ 0,
-      home_score ==  away_score~ .5
-    )
+      home_score ==  away_score~ .5)
   ) 
 
 nfl |>
@@ -46,18 +45,16 @@ spread_win <- nfl |>
 home_win |> left_join(spread_win, by = 'season') |> View()
 
 K <- 32
+initial_ratings <- make_init_ratings(nfl)
+
 
 nfl <- nfl |>
-  filter(season == 2022) 
+  filter(season == 2022) |>
+  rename(team_a = home_team, team_b = away_team)
 
-#initial_ratings <- data.frame(
-#  team = distinct(nfl, team_a),
-#  elo = 1500
-#)
+weekly_ratings <- data.frame(week = integer(), team = character(), elo = numeric())
 
-
-make_init_ratings(nfl)
-
+# Loop through each row in the nfl data frame
 for (i in 1:nrow(nfl)) {
   team_a <- nfl$team_a[i]
   team_b <- nfl$team_b[i]
@@ -69,28 +66,17 @@ for (i in 1:nrow(nfl)) {
   expected_b <- 1 - expected_a
   new_elo_a <- elo_a + K * (outcome_a - expected_a)
   new_elo_b <- elo_b + K * (outcome_b - expected_b)
+  
+  # Store the ratings for this week in the weekly_ratings data frame
+  weekly_ratings <- rbind(weekly_ratings, data.frame(week = nfl$week[i], team = team_a, elo = new_elo_a))
+  weekly_ratings <- rbind(weekly_ratings, data.frame(week = nfl$week[i], team = team_b, elo = new_elo_b))
+  
+  # Update the ratings in the initial_ratings data frame
   initial_ratings$elo[initial_ratings$team == team_a] <- new_elo_a
   initial_ratings$elo[initial_ratings$team == team_b] <- new_elo_b
 }
 
-
-initial_ratings <- data.frame(
-  team = distinct(nfl, team_a),
-  elo = 1500
-)
-
-for (i in 1:nrow(nfl)) {
-  team_a <- nfl$team_a[i]
-  team_b <- nfl$team_b[i]
-  elo_a <- initial_ratings$elo[initial_ratings$team == team_a]
-  elo_b <- initial_ratings$elo[initial_ratings$team == team_b]
-  point_diff <- nfl$result[i] %>% abs() # Assuming you have a column for point differential
-  outcome_a <- nfl$outcome[i]
-  outcome_b <- 1 - outcome_a 
-  expected_a <- calculate_expected_outcome(elo_a, elo_b, point_diff)
-  expected_b <- 1 - expected_a
-  new_elo_a <- elo_a + K * (outcome_a - expected_a)
-  new_elo_b <- elo_b + K * (outcome_b - expected_b)
-  initial_ratings$elo[initial_ratings$team == team_a] <- new_elo_a
-  initial_ratings$elo[initial_ratings$team == team_b] <- new_elo_b
-}
+#initial_ratings <- data.frame(
+#  team = distinct(nfl, team_a),
+#  elo = 1500
+#)
